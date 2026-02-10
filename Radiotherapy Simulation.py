3934271ba2f53351ceb3edff17a043ff7604b78f
SpeedyHonda <samuel.j.a.oconnor@gmail.com>
2026-02-10 00:11:19 +0000

    Added Beam Shapes

--- Radiotherapy Simulation.py
+++ Radiotherapy Simulation.py
@@ -1,6 +1,7 @@
 # Imports
 import numpy as np
 import random
+
 # Classes
 class Vector3():
     def __init__(self, x_input, y_input, z_input):
@@ -9,10 +10,14 @@ class Vector3():
         self.z = z_input
 
     def normalise(self):
-        pass
+        sum = (self.x**2 + self.y**2 + self.z**2)**(1/2)
+        self.x /= sum
+        self.y /= sum
+        self.z /= sum
     
     def __str__(self):
         return f"{self.x}, {self.y}, {self.z}"
+    
 # Water Phantom
 length, width, height = float(input()), float(input()), float(input())
 voxelSize = float(input())
@@ -32,11 +37,32 @@ print(x)
 print(y)
 print(z)
 
-def initialise_particles(number_of_particles):
-    return {i : {"Position" : Vector3(0,0,0), "Rotation" : Vector3(0,0,0), "Energy Level" : energy} for i in range(number_of_particles)}
+def get_beam_position(beam_shape, length, width):
+    if beam_shape == "circle":
+        diameter = min(length, width) / 2
+        radius = diameter / 2
+        distance = random.random() * radius
+        theta = random.random() * 2 * np.pi
+        y = np.sin(theta) * distance
+        x = np.cos(theta) * distance
+    else:
+        raise ValueError("Beam Shape is invalid")
 
-particles = initialise_particles(100)
+    return x, y
+
+def initialise_particles(number_of_particles, beam_type):
+    particles = {}
+
+    for i in range(number_of_particles):
+        x0, y0 = get_beam_position(beam_type, length, width)
+        z0 = 0 
+
+        particles[i] = {"Position": Vector3(x0, y0, z0), "Rotation": Vector3(0, 0, 1), "Energy Level": energy}
+
+    return particles
+
+
+particles = initialise_particles(100, "circle")
 print(particles[0]["Position"])
 
-diameter = min(length,width,height)/2
-centre = (nx/2, ny/2)
+# I am defining rotation in a Vector3 as x degrees about the x axis, y degrees about the y axis and z degrees about the z axis
